e66157579d979d69492e8232bc2573d9ed521dd8
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-08 15:37:03 +0200

    Importerat routes för navlänkar och active-komponent.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  // Importerar routes för länkar + active links
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
