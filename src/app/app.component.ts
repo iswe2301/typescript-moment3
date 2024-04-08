f057f3b37119bda4c289dc1c5e25de9eab6a8fb4
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-08 18:13:49 +0200

    Inkludeat mobilmeny som komponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // Importerar komponenter för header och footer
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MobileMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
