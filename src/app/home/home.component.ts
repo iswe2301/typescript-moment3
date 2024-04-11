e094e9b881a746f14a496db7286f812c0a8bd80e
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-11 20:23:59 +0200

    Importerat RouterLink, skapat egenskaper för bild och HTML-sträng. Skapat metod för att byta bild och text vid klick.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  // Skapar egenskaper av typen sträng och tilldelar bildens sökväg som värdet samt kattens prat i HTML
+  catImage: string = "assets/images/cat1.png";
+  catTalk: string = "<strong>PSSST...<br>Klicka på Mej, så säger jag Hej!</strong>";
+
+  // Metod för att byta bild
+  toggleImage(): void {
+    // Kontrollerar om bilden är bild 1, byter till bild 2 och ändrar kattens prat
+    if (this.catImage == "assets/images/cat1.png") {
+      this.catImage = "assets/images/cat2.png";
+      this.catTalk = "<strong>Hej på dig, hoppas du får en Majuig dag!</strong>";
+      // Kontrollerar om bilden är bild 2, byter till bild 1 och ändrar kattens prat
+    } else if (this.catImage = "assets/images/cat2.png") {
+      this.catImage = "assets/images/cat1.png";
+      this.catTalk = "<strong>PSSST!<br>Klicka på Mej, så säger jag Hej!</strong>";
+    }
+  }
 }
